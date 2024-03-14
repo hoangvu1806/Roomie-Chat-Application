d363c63ffe394e52049bad6793da10ace1cf91ba
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-03-14 20:32:10 +0700

    updata

--- src/com/hoangvu/main/Main.java
+++ src/com/hoangvu/main/Main.java
@@ -2,12 +2,12 @@ package com.hoangvu.main;
 
 import com.hoangvu.component.PanelCover;
 
-import java.awt.*;
+import com.hoangvu.component.PanelLoginAndRegister;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
-
-import com.hoangvu.component.PanelLoginAndRegister;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import net.miginfocom.swing.MigLayout;
 import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.TimingTarget;
@@ -15,108 +15,118 @@ import org.jdesktop.animation.timing.TimingTargetAdapter;
 
 public class Main extends javax.swing.JFrame {
 
+    private final DecimalFormat df = new DecimalFormat("##0.###", DecimalFormatSymbols.getInstance(Locale.US));
     private MigLayout layout;
     private PanelCover cover;
     private PanelLoginAndRegister loginAndRegister;
-    private boolean isLogin;
+    private boolean isLogin = true;
     private final double addSize = 30;
     private final double coverSize = 40;
     private final double loginSize = 60;
-    private final DecimalFormat df = new DecimalFormat("##0.###");
 
     public Main() {
         initComponents();
         init();
     }
-    private void init(){
+
+    private void init() {
         layout = new MigLayout("fill, insets 0");
         cover = new PanelCover();
-
         loginAndRegister = new PanelLoginAndRegister();
         TimingTarget target = new TimingTargetAdapter() {
             @Override
-            public void timingEvent(float fraction){
+            public void timingEvent(float fraction) {
                 double fractionCover;
                 double fractionLogin;
                 double size = coverSize;
-                if(fraction <= 0.5f){
+                if (fraction <= 0.5f) {
                     size += fraction * addSize;
-                }else{
+                } else {
                     size += addSize - fraction * addSize;
                 }
-                if(isLogin){
+                if (isLogin) {
                     fractionCover = 1f - fraction;
                     fractionLogin = fraction;
-
+                    if (fraction >= 0.5f) {
+                        cover.registerRight(fractionCover * 100);
+                    } else {
+                        cover.loginRight(fractionLogin * 100);
+                    }
                 } else {
                     fractionCover = fraction;
                     fractionLogin = 1f - fraction;
+                    if (fraction <= 0.5f) {
+                        cover.registerLeft(fraction * 100);
+                    } else {
+                        cover.loginLeft((1f - fraction) * 100);
+                    }
                 }
-
-                if(fraction >= 0.5f){
+                if (fraction >= 0.5f) {
                     loginAndRegister.showRegister(isLogin);
                 }
-                fractionCover = Double.valueOf(df.format((fractionCover)));
-                fractionLogin = Double.valueOf(df.format((fractionLogin)));
+                fractionCover = Double.valueOf(df.format(fractionCover));
+                fractionLogin = Double.valueOf(df.format(fractionLogin));
                 layout.setComponentConstraints(cover, "width " + size + "%, pos " + fractionCover + "al 0 n 100%");
                 layout.setComponentConstraints(loginAndRegister, "width " + loginSize + "%, pos " + fractionLogin + "al 0 n 100%");
                 bg.revalidate();
             }
 
             @Override
-            public void end(){
+            public void end() {
                 isLogin = !isLogin;
             }
         };
-        Animator animator = new Animator(1000, target);
+        Animator animator = new Animator(800, target);
         animator.setAcceleration(0.5f);
         animator.setDeceleration(0.5f);
-        animator.setResolution(0); //for smooths animation
+        animator.setResolution(0);  //  for smooth animation
         bg.setLayout(layout);
-        bg.add(cover,"width " + coverSize + "%, pos 0al 0 n 100%");
-        bg.add(loginAndRegister,"width " + loginSize + "%, pos 1al 0 n 100%" );
+        bg.add(cover, "width " + coverSize + "%, pos " + (isLogin ? "1al" : "0al") + " 0 n 100%");
+        bg.add(loginAndRegister, "width " + loginSize + "%, pos " + (isLogin ? "0al" : "1al") + " 0 n 100%"); //  1al as 100%
+        loginAndRegister.showRegister(!isLogin);
+        cover.login(isLogin);
         cover.addEvent(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                if (!animator.isRunning()){
+                if (!animator.isRunning()) {
                     animator.start();
                 }
             }
         });
     }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        bg = new javax.swing.JPanel();
+        bg = new javax.swing.JLayeredPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setName("frame0"); // NOI18N
         setUndecorated(true);
-        setType(java.awt.Window.Type.UTILITY);
 
         bg.setBackground(new java.awt.Color(255, 255, 255));
+        bg.setOpaque(true);
 
         javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
         bg.setLayout(bgLayout);
         bgLayout.setHorizontalGroup(
-            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 920, Short.MAX_VALUE)
+                bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 933, Short.MAX_VALUE)
         );
         bgLayout.setVerticalGroup(
-            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 520, Short.MAX_VALUE)
+                bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 537, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(bg, javax.swing.GroupLayout.Alignment.TRAILING)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(bg)
         );
 
         pack();
@@ -156,6 +166,6 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel bg;
+    private javax.swing.JLayeredPane bg;
     // End of variables declaration//GEN-END:variables
 }
