1f3dc83769417bc9ee0e4874e57410c783192cea
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-03-15 23:36:57 +0700

    add round panel

--- src/com/hoangvu/swing/PanelRound.java
+++ src/com/hoangvu/swing/PanelRound.java
@@ -1,23 +1,24 @@
+
 package com.hoangvu.swing;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.RenderingHints;
 
 public class PanelRound extends JPanel {
-
-    public PanelRound() {
+    public PanelRound(){
         setOpaque(false);
+
     }
 
     @Override
-    protected void paintComponent(Graphics grphcs) {
-        Graphics2D g2 = (Graphics2D) grphcs;
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(new Color(255, 255, 255));
-        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
-        super.paintComponent(grphcs);
+        g2.setColor(new Color(255,255,255));
+        g2.fillRoundRect(0,0,getWidth(),getHeight(),20,20);
+        super.paintComponent(g);
     }
 }
