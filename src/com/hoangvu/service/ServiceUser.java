b146ea63dfeaa2281d5eb64c54f1414409a02332
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-03-25 21:59:43 +0700

    add ServiceUser.java

---
+++ src/com/hoangvu/service/ServiceUser.java
@@ -0,0 +1,115 @@
+package com.hoangvu.service;
+
+import com.hoangvu.connection.DatabaseConnection;
+import com.hoangvu.model.BCrypt;
+import com.hoangvu.model.ModelUser;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class ServiceUser {
+    private final Connection con;
+
+    public ServiceUser() {
+        con = DatabaseConnection.getInstance().getConnection();
+    }
+
+    public void insertUser(ModelUser user) throws SQLException {
+
+        PreparedStatement p = con.prepareStatement(" insert into `usermanager` (UserName, Email, Password, VerifyCode, Permission) values(?,?,?,?,?)"
+                , PreparedStatement.RETURN_GENERATED_KEYS);
+        String verifycode = generateVerifyCode();
+        System.out.println("Verify code of " + user.getUserName() + ": " + verifycode);
+        String passHashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(4));
+        p.setString(1, user.getUserName());
+        p.setString(2, user.getEmail());
+        p.setString(3, passHashed);
+        p.setString(4, verifycode);
+        p.setString(5, "user");
+
+
+        p.execute();
+        ResultSet r = p.getGeneratedKeys();
+        r.first();
+        int userID = r.getInt(1);
+        r.close();
+        p.close();
+        user.setUserID(userID);
+        user.setVerifyCode(verifycode);
+
+    }
+
+    public String generateVerifyCode() throws SQLException {
+        DecimalFormat df = new DecimalFormat("000000");
+        Random ran = new Random();
+        String verifyCode = df.format(ran.nextInt(1000000));
+        while (checkDuplicateCode(verifyCode)) {
+            verifyCode = df.format(ran.nextInt(1000000));
+        }
+        return verifyCode;
+    }
+
+    public void doneVerify(int userID) throws SQLException {
+        PreparedStatement p = con.prepareStatement("UPDATE `usermanager` SET VerifyCode = '', Status = 'verified' WHERE UserID=? limit 1");
+        p.setInt(1, userID);
+        p.executeUpdate();
+        p.close();
+    }
+
+    private boolean checkDuplicateCode(String verifyCode) throws SQLException {
+        boolean duplicate = false;
+        try (PreparedStatement p = con.prepareStatement("SELECT UserID FROM `usermanager` WHERE VerifyCode=? LIMIT 1", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+            p.setString(1, verifyCode);
+            try (ResultSet r = p.executeQuery()) {
+                if (r.next()) { // Kiểm tra xem tập kết quả có ít nhất một bản ghi không
+                    duplicate = true;
+                }
+            }
+        }
+        return duplicate;
+    }
+
+    public boolean checkDuplicateUser(String userName) throws SQLException {
+        boolean duplicate = false;
+        try (PreparedStatement p = con.prepareStatement("SELECT UserID FROM `usermanager` WHERE UserName=? AND `Status` = 'verified'", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+            p.setString(1, userName);
+            try (ResultSet r = p.executeQuery()) {
+                if (r.first()) {
+                    duplicate = true;
+                }
+            }
+        }
+        return duplicate;
+    }
+
+    public boolean checkDuplicateEmail(String email) throws SQLException {
+        boolean duplicate = false;
+        try (PreparedStatement p = con.prepareStatement("SELECT UserID FROM `usermanager` WHERE Email=? AND `Status` = 'verified'", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+            p.setString(1, email);
+            try (ResultSet r = p.executeQuery()) {
+                if (r.first()) {
+                    duplicate = true;
+                }
+            }
+        }
+        return duplicate;
+    }
+
+    public boolean verifyCodeWithUser(int userID, String verifyCode) throws SQLException {
+        boolean verify = false;
+        try (PreparedStatement p = con.prepareStatement("SELECT UserID FROM `usermanager` WHERE UserID=? AND VerifyCode=? LIMIT 1", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+            p.setInt(1, userID);
+            p.setString(2, verifyCode);
+            try (ResultSet r = p.executeQuery()) {
+                if (r.first()) {
+                    verify = true;
+                }
+            }
+        }
+        return verify;
+    }
+}
