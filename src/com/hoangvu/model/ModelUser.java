681410a75987c83acfde093b6eb234a7f5fc26c2
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-03-25 22:03:50 +0700

    Updata ModelUser.java

--- src/com/hoangvu/model/ModelUser.java
+++ src/com/hoangvu/model/ModelUser.java
@@ -59,6 +59,67 @@ public class ModelUser {
 
     public ModelUser() {
     }
+    public  boolean isValidEmail() {
+        if (email == null) {
+            return false;
+        }
+        if (email.length() < 5) {
+            return false;
+        }
+        int atIndex = email.indexOf('@');
+        if (atIndex == -1 || atIndex == 0 || atIndex == email.length() - 1) {
+            return false;
+        }
+        int dotIndex = email.lastIndexOf('.');
+        if (dotIndex == -1 || dotIndex < atIndex + 2 || dotIndex == email.length() - 1) {
+            return false;
+        }
+        for (int i = atIndex + 1; i < dotIndex; i++) {
+            char ch = email.charAt(i);
+            if (!Character.isLetterOrDigit(ch) && ch != '-') {
+                return false;
+            }
+        }
+        char lastDomainChar = email.charAt(dotIndex + 1);
+        if (!Character.isLetter(lastDomainChar)) {
+            return false;
+        }
+        return true;
+    }
+    public boolean isValidUsername() {
+        if (userName.length() < 6) {
+            return false;
+        }
+        for (char ch : userName.toCharArray()) {
+            if (!(Character.isLetter(ch) || ch == '.' || ch == '_' || Character.isDigit(ch))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isValidPassword() {
+        if (password.length() < 8) {
+            return false;
+        }
+        int countLowercase = 0;
+        int countUppercase = 0;
+        int countDigit = 0;
+        int countSpecial = 0;
+        for (char c : password.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                countLowercase++;
+            }
+            if (c >= 'A' && c <= 'Z') {
+                countUppercase++;
+            }
+            if (c >= '0' && c <= '9') {
+                countDigit++;
+            }
+        }
+        return countLowercase >= 1 && countUppercase >= 1 && countDigit >= 1;
+    }
+
+
 
     private int userID;
     private String userName;
