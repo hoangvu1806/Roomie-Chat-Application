bf2c52f1f62c71432221a10230df0f9b4a8a200d
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-03-31 20:29:30 +0700

    Updata ModelLogin.java

--- src/com/hoangvu/model/ModelLogin.java
+++ src/com/hoangvu/model/ModelLogin.java
@@ -2,5 +2,25 @@
 package com.hoangvu.model;
 
 public class ModelLogin {
-    
+    public ModelLogin(){
+
+    }
+    public ModelLogin(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    private String email;
+    private String password;
 }
