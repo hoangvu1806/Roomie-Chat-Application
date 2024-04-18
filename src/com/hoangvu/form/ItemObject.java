3b641c55abfe1ad1f562783d4a1fc0f6a4501140
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-04-18 23:17:29 +0700

    Updata Chat.java

--- src/com/hoangvu/form/ItemObject.java
+++ src/com/hoangvu/form/ItemObject.java
@@ -8,12 +8,12 @@ package com.hoangvu.form;
  *
  * @author Admin
  */
-public class Chat extends javax.swing.JPanel {
+public class ItemObject extends javax.swing.JPanel {
 
     /**
-     * Creates new form Chat
+     * Creates new form ItemObject
      */
-    public Chat() {
+    public ItemObject() {
         initComponents();
     }
 
@@ -26,31 +26,19 @@ public class Chat extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Chat Body");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
-                .addGap(32, 32, 32))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(184, 184, 184)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 101, Short.MAX_VALUE)
-                .addGap(215, 215, 215))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/com/hoangvu/form/MenuButton.java
@@ -0,0 +1,10 @@
+
+package com.hoangvu.form;
+
+/**
+ *
+ * @author Hoang Vu
+ */
+public class MenuButton {
+    
+}
