01aabd19758e5cf0d44d0bf721313f8c5ee880ae
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-04-18 23:18:25 +0700

    Initialize ItemObject.java

--- src/com/hoangvu/form/ItemObject.java
+++ src/com/hoangvu/form/ItemObject.java
@@ -13,8 +13,9 @@ public class ItemObject extends javax.swing.JPanel {
     /**
      * Creates new form ItemObject
      */
-    public ItemObject() {
+    public ItemObject(String name) {
         initComponents();
+        lbUserName.setText(name);
     }
 
     /**
@@ -26,19 +27,42 @@ public class ItemObject extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        imageAvatar1 = new com.hoangvu.swing.ImageAvatar();
+        lbUserName = new javax.swing.JLabel();
+
+        setBackground(new java.awt.Color(204, 204, 204));
+
+        imageAvatar1.setBorderSize(0);
+        imageAvatar1.setImage(new javax.swing.ImageIcon(getClass().getResource("/com/hoangvu/icon/avatar-45.png"))); // NOI18N
+        imageAvatar1.setOpaque(true);
+
+        lbUserName.setFont(new java.awt.Font("JetBrains Mono", 1, 14)); // NOI18N
+        lbUserName.setText("User name");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(imageAvatar1, javax.swing.GroupLayout.DEFAULT_SIZE, 43, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lbUserName, javax.swing.GroupLayout.DEFAULT_SIZE, 119, Short.MAX_VALUE)
+                .addContainerGap(26, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lbUserName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(imageAvatar1, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE))
+                .addGap(0, 0, 0))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.hoangvu.swing.ImageAvatar imageAvatar1;
+    private javax.swing.JLabel lbUserName;
     // End of variables declaration//GEN-END:variables
 }
