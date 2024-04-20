f0e4b1f4d144ed2adbaae651093d7d7ef7c8d531
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-04-20 23:47:19 +0700

    Initialize ChatTittle.java

--- src/com/hoangvu/form/ChatTittle.java
+++ src/com/hoangvu/form/ChatTittle.java
@@ -34,7 +34,7 @@ public class ChatTittle extends javax.swing.JPanel {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 40, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
