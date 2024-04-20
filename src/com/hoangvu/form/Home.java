ffd91484297dbc3358ade34fe8aa551f39a28a68
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-04-20 23:42:26 +0700

    Updata Home.java

--- src/com/hoangvu/form/Home.java
+++ src/com/hoangvu/form/Home.java
@@ -9,12 +9,15 @@ public class Home extends javax.swing.JLayeredPane {
         init();
     }
     private void init(){
-        setLayout(new MigLayout("fillx, filly", "0[44!]0[200!]5[fill, 100%]5[200!]5", "0[fill]0"));
-        this.add(new ToolBar());
-        this.add(new MenuLeft());
+        this.menuLeft = new MenuLeft();
+        this.menuRight = new MenuRight();
+        this.toolBar = new ToolBar(this.menuLeft);
         this.chat = new Chat();
+        setLayout(new MigLayout("fillx, filly", "0[44!]0[200!]5[fill, 100%]5[200!]5", "0[fill]0"));
+        this.add(this.toolBar);
+        this.add(this.menuLeft);
         this.add(this.chat);
-        this.add(new MenuRight());
+        this.add(this.menuRight);
     }
 
     @SuppressWarnings("unchecked")
@@ -34,6 +37,9 @@ public class Home extends javax.swing.JLayeredPane {
     }// </editor-fold>//GEN-END:initComponents
 
     private Chat chat;
+    private MenuLeft menuLeft;
+    private ToolBar toolBar;
+    private MenuRight menuRight;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
