e807ea17efe5f9249d1ff0fb9af33c77c4fa64f7
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-04-29 23:15:10 +0700

    Initialize Service.java

--- src/com/hoangvu/service/Service.java
+++ src/com/hoangvu/service/Service.java
@@ -1,6 +1,34 @@
-
 package com.hoangvu.service;
 
+
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
 public class Service {
-    
+
+    private static Service instance;
+    private Socket client;
+    private final int PORT_NUMBER = 9999;
+    private final String IP_ADDRESS = "127.0.0.1";
+
+    public static Service getInstance() {
+        if (instance == null) {
+            instance = new Service();
+        }
+        return instance;
+    }
+    private Service() {
+
+    }
+
+    public void startServer() {
+        try{
+            client = IO.socket("http://"+IP_ADDRESS+":"+PORT_NUMBER);
+            client.open();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
 }
