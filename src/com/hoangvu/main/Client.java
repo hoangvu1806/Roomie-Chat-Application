5ded89645dffba172ac023e3cc1390d9dfbdccdc
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-04-30 23:23:19 +0700

    Updata Clinet.java

--- src/com/hoangvu/main/Client.java
+++ src/com/hoangvu/main/Client.java
@@ -39,7 +39,6 @@ public class Client extends javax.swing.JFrame {
         viewImage.setVisible(false);
         home.setVisible(true);
         initEvent();
-        Service.getInstance().startServer();
     }
 
     public void initEvent() {
