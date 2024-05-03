da1e762051fec8357cb763bcfe29631edf295c3c
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-03 23:15:32 +0700

    Updata ModelLogin.java

--- src/com/hoangvu/model/ModelLogin.java
+++ src/com/hoangvu/model/ModelLogin.java
@@ -1,6 +1,9 @@
 
 package com.hoangvu.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ModelLogin {
     public ModelLogin(){
 
@@ -8,6 +11,17 @@ public class ModelLogin {
     public ModelLogin(String email, String password){
         this.email = email;
         this.password = password;
+    }
+    public JSONObject toJsonObject() throws JSONException {
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("email", email);
+            jsonObject.put("password", password);
+            return jsonObject;
+        } catch (JSONException je){
+            System.out.println(je);
+            return null;
+        }
     }
     public String getEmail(){
         return this.email;
