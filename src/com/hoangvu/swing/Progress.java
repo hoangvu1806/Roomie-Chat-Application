f6abf4c633dc52f676ed44127542128780dd070b
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-03 23:16:40 +0700

    Initialize Progress.java

--- src/com/hoangvu/swing/Progress.java
+++ src/com/hoangvu/swing/Progress.java
@@ -1,11 +1,26 @@
+
 package com.hoangvu.swing;
 
-import javax.swing.JProgressBar;
+
+import javax.swing.*;
 
 public class Progress extends JProgressBar {
+    public ProgressType getProgressType() {
+        return progressType;
+    }
 
+    public void setProgressType(ProgressType progressType) {
+        this.progressType = progressType;
+        repaint();
+    }
+
+    private ProgressType progressType = ProgressType.NON;
     public Progress() {
         setOpaque(false);
         setUI(new ProgressCircleUI(this));
     }
+    public static enum ProgressType {
+        NON, DOWNLOAD, CANCEL, FILE;
+    }
+
 }
