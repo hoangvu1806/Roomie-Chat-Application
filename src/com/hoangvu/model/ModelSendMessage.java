d12bff5cf96e1e70feaeb5da623c02370ed94bc1
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-09 23:14:06 +0700

    Initialize ModelSendMessage.java

--- src/com/hoangvu/model/ModelSendMessage.java
+++ src/com/hoangvu/model/ModelSendMessage.java
@@ -1,6 +1,57 @@
 
 package com.hoangvu.model;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ModelSendMessage {
-    
+
+    public int getFromUserID() {
+        return fromUserID;
+    }
+
+    public void setFromUserID(int fromUserID) {
+        this.fromUserID = fromUserID;
+    }
+
+    public int getToUserID() {
+        return toUserID;
+    }
+
+    public void setToUserID(int toUserID) {
+        this.toUserID = toUserID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ModelSendMessage(int fromUserID, int toUserID, String text) {
+        this.fromUserID = fromUserID;
+        this.toUserID = toUserID;
+        this.text = text;
+    }
+
+    public ModelSendMessage() {
+    }
+
+    private int fromUserID;
+    private int toUserID;
+    private String text;
+
+    public JSONObject toJsonObject() {
+        try {
+            JSONObject json = new JSONObject();
+            json.put("fromUserID", fromUserID);
+            json.put("toUserID", toUserID);
+            json.put("text", text);
+            return json;
+        } catch (JSONException e) {
+            return null;
+        }
+    }
 }
