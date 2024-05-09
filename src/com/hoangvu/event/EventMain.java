af8d5f3168f0516cf2f8f9f9bd6e99ccc60f7443
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-09 23:19:32 +0700

    Initialize EvenMain.java

--- src/com/hoangvu/event/EventMain.java
+++ src/com/hoangvu/event/EventMain.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.hoangvu.event;
 
-/**
- *
- * @author Admin
- */
-public class EventMain {
-    
+import com.hoangvu.model.ModelUser;
+
+public interface EventMain {
+
+    public void showLoading(boolean show);
+
+    public void initChat();
+
+    public void selectUser(ModelUser user);
+    public void updataUser(ModelUser user);
+
 }
