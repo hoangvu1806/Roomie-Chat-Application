5a57c4ca3430bf9b4d47f3d74a515c252b1b64ee
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-09 23:19:56 +0700

    Initialize EventMenuleft.java

--- src/com/hoangvu/event/EventMenuLeft.java
+++ src/com/hoangvu/event/EventMenuLeft.java
@@ -2,11 +2,13 @@
 package com.hoangvu.event;
 
 import com.hoangvu.model.ModelUser;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public interface EventMenuLeft {
 
-    public void newUser(List<ModelUser> users);
+    public void newUser(ArrayList<ModelUser> users);
 
     public void userConnect(int userID);
 
