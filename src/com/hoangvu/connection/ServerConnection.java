82b22059f55ef111d9c3e97a7e379c0ef6fd38e6
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-09 23:19:00 +0700

    Initialize ServerConnection.java

--- src/com/hoangvu/connection/ServerConnection.java
+++ src/com/hoangvu/connection/ServerConnection.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.hoangvu.connection;
 
-/**
- *
- * @author Admin
- */
+import com.hoangvu.event.PublicEvent;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+import java.net.URISyntaxException;
+
 public class ServerConnection {
-    
+    private static ServerConnection instance;
+    private Socket client;
+    private final int PORT_NUMBER = 9999;
+    private final String IP_ADDRESS = "localhost";
+
+    private ServerConnection() {
+        try {
+            client = IO.socket("http://" + IP_ADDRESS + ":" + PORT_NUMBER);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ServerConnection getInstance() {
+        if (instance == null) {
+            instance = new ServerConnection();
+        }
+        return instance;
+    }
+
+    public Socket getClient() {
+        return client;
+    }
+
+    public void connect() {
+        client.on("user-status", new Emitter.Listener() {
+            @Override
+            public void call(Object... objects) {
+                int userID = (Integer) objects[0];
+                boolean status = (Boolean) objects[1];
+                if (status) {
+                    //  connect
+                    PublicEvent.getInstance().getEventMenuLeft().userConnect(userID);
+                } else {
+                    //  disconnect
+                    PublicEvent.getInstance().getEventMenuLeft().userDisconnect(userID);
+                }
+            }
+        });
+        client.open();
+    }
+
+    public void disconnect() {
+        client.disconnect();
+    }
 }
