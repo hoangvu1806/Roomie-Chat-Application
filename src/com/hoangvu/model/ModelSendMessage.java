8247ab7f27fd60853324e2a593ef4b124690b9f7
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-10 23:49:59 +0700

    Updata

--- src/com/hoangvu/model/ModelSendMessage.java
+++ src/com/hoangvu/model/ModelSendMessage.java
@@ -21,34 +21,47 @@ public class ModelSendMessage {
     public void setToUserID(int toUserID) {
         this.toUserID = toUserID;
     }
-
-    public String getText() {
-        return text;
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String text) {
+        this.message = text;
+    }
+    public Object getImage() {
+        return image;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public void setImage(Object image) {
+        this.image = image;
     }
 
     public ModelSendMessage(int fromUserID, int toUserID, String text) {
         this.fromUserID = fromUserID;
         this.toUserID = toUserID;
-        this.text = text;
+        this.message = text;
+    }
+    public ModelSendMessage(int fromUserID, int toUserID, Object image) {
+        this.fromUserID = fromUserID;
+        this.toUserID = toUserID;
+        this.image = image;
     }
 
     public ModelSendMessage() {
     }
 
+
+    private Object image;
+    private Object file;
     private int fromUserID;
     private int toUserID;
-    private String text;
+    private String message;
 
     public JSONObject toJsonObject() {
         try {
             JSONObject json = new JSONObject();
             json.put("fromUserID", fromUserID);
             json.put("toUserID", toUserID);
-            json.put("text", text);
+            json.put("message", message);
             return json;
         } catch (JSONException e) {
             return null;
