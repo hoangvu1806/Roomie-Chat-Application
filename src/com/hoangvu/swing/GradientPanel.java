5ebe8af90c2013a725ccb5b03c9b31be9b24d865
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-11 10:58:13 +0700

    Initialize

--- src/com/hoangvu/swing/GradientPanel.java
+++ src/com/hoangvu/swing/GradientPanel.java
@@ -12,6 +12,25 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class GradientPanel extends JPanel {
+    public Color getColor1() {
+        return color1;
+    }
+
+    public void setColor1(Color color1) {
+        this.color1 = color1;
+    }
+
+    private Color color1 = new Color(0, 0, 0, 255);
+
+    public Color getColor2() {
+        return color2;
+    }
+
+    public void setColor2(Color color2) {
+        this.color2 = color2;
+    }
+
+    private Color color2 = new Color(255, 255, 255);
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -19,8 +38,6 @@ public class GradientPanel extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         int w = getWidth(), h = getHeight();
-        Color color1 = new Color(1,1,1);
-        Color color2 = Color.GREEN;
         GradientPaint gp = new GradientPaint(0, 0, color1, 0, h, color2);
         g2d.setPaint(gp);
         g2d.fillRect(0, 0, w, h);
