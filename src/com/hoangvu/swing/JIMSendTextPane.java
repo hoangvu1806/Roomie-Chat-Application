38cf90934cbdbb91d7cf106603efb76aec7642ac
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-11 10:57:37 +0700

    Initialize

--- src/com/hoangvu/swing/JIMSendTextPane.java
+++ src/com/hoangvu/swing/JIMSendTextPane.java
@@ -11,9 +11,19 @@ import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
 import javax.swing.text.View;
 import javax.swing.text.ViewFactory;
+import java.awt.*;
 
 public class JIMSendTextPane extends JTextPane {
 
+    public void setHintText(String hintText) {
+        this.hintText = hintText;
+    }
+
+    public String getHintText() {
+        return hintText;
+    }
+
+    private String hintText="";
     private class WarpEditorKit extends StyledEditorKit {
 
         private ViewFactory defaultFactory = new WarpColumnFactory();
@@ -71,5 +81,21 @@ public class JIMSendTextPane extends JTextPane {
     public JIMSendTextPane() {
         super();
         this.setEditorKit(new WarpEditorKit());
+    }
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        if (getText().length() == 0 && !hintText.equals("")) {
+            int h = getHeight();
+            ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            Insets ins = getInsets();
+            FontMetrics fm = g.getFontMetrics();
+            int c0 = getBackground().getRGB();
+            int c1 = getForeground().getRGB();
+            int m = 0xfefefefe;
+            int c2 = ((c0 & m) >>> 1) + ((c1 & m) >>> 1);
+            g.setColor(new Color(c2, true));
+            g.drawString(hintText, ins.left, h / 2 + fm.getAscent() / 2 - 2);
+        }
     }
 }
