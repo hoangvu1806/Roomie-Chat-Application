47034a77083a016c1118b4ebfa9d85b2269e3aee
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-13 20:49:11 +0700

    Updata

--- src/com/hoangvu/form/ChatBottom.java
+++ src/com/hoangvu/form/ChatBottom.java
@@ -1,6 +1,7 @@
 package com.hoangvu.form;
 
 
+import com.hoangvu.component.PanelMore;
 import com.hoangvu.connection.ServerConnection;
 import com.hoangvu.event.EventChat;
 import com.hoangvu.event.PublicEvent;
@@ -27,19 +28,24 @@ public class ChatBottom extends javax.swing.JPanel {
     public ModelUser getUser() {
         return user;
     }
+
     public void setUser(ModelUser user) {
         this.user = user;
     }
+
     public void setToUser(ModelUser toUser) {
         this.toUser = toUser;
     }
+
     public ChatBottom(ModelUser user) {
         this.user = user;
         initComponents();
         init();
     }
-    public void init(){
-        setLayout(new MigLayout("fillx, filly","0[fill]0[]0[]2","2[fill]2"));
+
+    public void init() {
+        mig = new MigLayout("fillx, filly", "0[fill]0[]0[]2", "2[fill]2[]0");
+        setLayout(mig);
         JScrollPane scroll = new JScrollPane();
         scroll.setBorder(null);
         JIMSendTextPane sendTextPane = new JIMSendTextPane();
@@ -52,14 +58,14 @@ public class ChatBottom extends javax.swing.JPanel {
         sendTextPane.setHintText("Type a message...");
         scroll.setViewportView(sendTextPane);
         ScrollBar sb = new ScrollBar();
-        sb.setPreferredSize(new Dimension(2,10));
+        sb.setPreferredSize(new Dimension(2, 10));
         scroll.setVerticalScrollBar(sb);
         add(sb);
-        add(scroll,"w 100%");
+        add(scroll, "w 100%");
 
         JPanel panel = new JPanel();
-        panel.setLayout(new MigLayout("filly","0[]0","2[bottom]2"));
-        panel.setPreferredSize(new Dimension(30,28));
+        panel.setLayout(new MigLayout("filly", "0[]3[]0", "2[bottom]2"));
+        panel.setPreferredSize(new Dimension(30, 28));
         panel.setBackground(Color.WHITE);
 
         JButton sendMsBt = new JButton();
@@ -73,10 +79,10 @@ public class ChatBottom extends javax.swing.JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String message = sendTextPane.getText().trim();
-                ModelSendMessage data = new ModelSendMessage(user.getUserID(),toUser.getUserID(), message);
-                if (!message.isEmpty()){
+                ModelSendMessage data = new ModelSendMessage(user.getUserID(), toUser.getUserID(), message);
+                if (!message.isEmpty()) {
                     // add chat items
-                    System.out.println("From "+ user.getUserID()+ "to "+ toUser.getUserID() +"\n");
+                    System.out.println("From " + user.getUserID() + "to " + toUser.getUserID() + "\n");
                     send(data);
                     PublicEvent.getInstance().getEventChat().sendMessage(data);
                     sendTextPane.setText("");
@@ -86,16 +92,50 @@ public class ChatBottom extends javax.swing.JPanel {
                     sendTextPane.grabFocus();
                 }
             }
+
         });
+
+        JButton moreBt = new JButton();
+        moreBt.setBorder(null);
+        moreBt.setOpaque(false);
+        moreBt.setContentAreaFilled(false);
+        moreBt.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        moreBt.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/com/hoangvu/icon/dis_more.png"))));
+        moreBt.setSelectedIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/com/hoangvu/icon/more.png"))));
+        moreBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (morePanel.isVisible()) {
+                    moreBt.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/com/hoangvu/icon/dis_more.png"))));
+                    morePanel.setVisible(false);
+                    mig.setComponentConstraints(morePanel, "dock south, h 0!");
+                    revalidate();
+                } else {
+                    moreBt.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/com/hoangvu/icon/more.png"))));
+                    morePanel.setVisible(true);
+                    mig.setComponentConstraints(morePanel, "dock south, h 125!");
+                    revalidate();
+                }
+
+            }
+        });
+
+        panel.add(moreBt);
         panel.add(sendMsBt);
-        add(panel);
+        add(panel,"wrap");
+        morePanel = new PanelMore();
+        morePanel.setVisible(false);
+        add(morePanel,"dock south, h 0!");
     }
+
     private void send(ModelSendMessage data) {
         ServerConnection.getInstance().getClient().emit("send-message", data.toJsonObject());
     }
+
     private void refresh() {
         revalidate();
     }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -103,16 +143,17 @@ public class ChatBottom extends javax.swing.JPanel {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 338, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 338, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 38, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 38, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
-
+    private PanelMore morePanel;
+    private MigLayout mig;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
