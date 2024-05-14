a7797d8fa115e7a224b6fb8f31b931d3807c031f
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-14 11:16:08 +0700

    Updata

--- src/com/hoangvu/form/ChatImage.java
+++ src/com/hoangvu/form/ChatImage.java
@@ -18,14 +18,12 @@ public class ChatImage extends javax.swing.JLayeredPane {
         setLayout(new MigLayout("", "0[" + (right ? "right" : "left") + "]0", "3[]3"));
     }
 
-    public void addImage(Icon... images) {
-        for (Icon image : images) {
-            PictureBox pic = new PictureBox();
-            pic.setPreferredSize(getAutoSize(image, 200, 200));
-            pic.setImage(image);
-            addEvent(pic, image);
-            add(pic, "wrap");
-        }
+    public void addImage(Icon image) {
+        PictureBox pic = new PictureBox();
+        pic.setPreferredSize(getAutoSize(image, 200, 200));
+        pic.setImage(image);
+        addEvent(pic, image);
+        add(pic, "wrap");
     }
     public void addImage(String[] images) {
         for (String  image : images) {
