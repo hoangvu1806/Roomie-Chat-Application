ad15c480ffccc16345674f9f8e1335690eb2d014
hoangvu1806 <dohoangvu.nt1806@gmail.com>
2024-05-18 23:17:45 +0700

    Updata

--- src/com/hoangvu/model/ImageSender.java
+++ src/com/hoangvu/model/ImageSender.java
@@ -1,22 +1,27 @@
 
 package com.hoangvu.model;
 
+import com.hoangvu.connection.ServerConnection;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
 
 public class ImageSender {
-    public String convertImageToBase64(String imagePath) {
+    public ImageSender(){}
+    public byte[] convertImageToBase64(String imagePath) {
         File file = new File(imagePath);
         try {
             BufferedImage bufferedImage = ImageIO.read(file);
             ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream();
             ImageIO.write(bufferedImage, "png", byteOutStream);
             byte[] bytes = byteOutStream.toByteArray();
-            return Base64.getEncoder().encodeToString(bytes);
+//            return Base64.getEncoder().encodeToString(bytes);
+            return bytes;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -24,9 +29,15 @@ public class ImageSender {
     }
 
     public void sendImage(String imagePath) {
-        String imageString = convertImageToBase64(imagePath);
+        byte[] imageString = convertImageToBase64(imagePath);
         if (imageString != null) {
-            ServerConnection.getInstance().getClient().emit("send-image", imageString);
+            System.out.println(imageString.toString().length());
         }
+//        ServerConnection.getInstance().getClient().emit("send-images", imageString);
+    }
+
+    public static void main(String[] args) {
+        ImageSender imageSender = new ImageSender();
+        imageSender.sendImage("E:\\Roomie Project\\src\\com\\hoangvu\\icon\\bb.png");
     }
 }
